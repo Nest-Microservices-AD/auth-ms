6391383ba9d130ef9037192474c1fb0a83c0420c
Andres Degano <andres.degano@gmail.com>
2024-12-31 19:49:18 +0100

    init microservice

---
+++ src/config/envs.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    NATS_SERVERS: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+if (error) {
+  throw new Error(`Env Vars error ${error.message}`);
+}
+
+const envValues: {
+  PORT: number;
+  NATS_SERVERS: string;
+} = value;
+
+export const envs = {
+  PORT: envValues.PORT,
+  NATS_SERVERS: envValues.NATS_SERVERS.split(','),
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,48 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import {
+  MicroserviceOptions,
+  RpcException,
+  Transport,
+} from '@nestjs/microservices';
+import {
+  HttpStatus,
+  Logger,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('authService');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        return new RpcException({
+          status: HttpStatus.BAD_REQUEST,
+          message: validationErrors.map((error) => ({
+            field: error.property,
+            error: Object.values(error.constraints).join(', '),
+          })),
+        });
+      },
+    }),
+  );
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.NATS_SERVERS,
+      },
+    },
+    { inheritAppConfig: true },
+  );
+  await app.startAllMicroservices();
+  await app.listen(envs.PORT || 3000);
+  logger.log(`AUTH Service started on port ${envs.PORT}`);
 }
 bootstrap();
